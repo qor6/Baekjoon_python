8527143394e8408f3518159ea3f98935702ace6c
qor6 <87318054+qor6@users.noreply.github.com>
2021-07-12 22:47:24 +0900

    7/12

---
+++ 2742.py
@@ -0,0 +1,8 @@
+n = int(input())
+
+for i in range(n,0,-1):
+    print(i+1)
+
+
+for i in reversed(range(n)):
+    print(i+1)
---
+++ 8393.py
@@ -0,0 +1,6 @@
+n=int(input())
+s = 0
+for i in range(n):
+    s += i+1
+print(b)
+    
